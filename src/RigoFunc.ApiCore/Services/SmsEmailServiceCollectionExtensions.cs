064ea13d776fbf02409331fe79d6574351b1526a
RigoFunc <rigofunc@outlook.com>
2016-06-16 13:06:07 +0800

    fix: fixed the naming issues

--- src/RigoFunc.ApiCore/Services/SmsEmailServiceCollectionExtensions.cs
+++ src/RigoFunc.ApiCore/Services/SmsEmailServiceCollectionExtensions.cs
@@ -4,7 +4,7 @@ using RigoFunc.ApiCore.Services;
 
 namespace Microsoft.Extensions.DependencyInjection {
     public static class SmsEmailServiceCollectionExtensions {
-        public static IServiceCollection AddSmlEmailServices(this IServiceCollection services, Action<ServiceOptions> setupAction) {
+        public static IServiceCollection AddSmsEmailService(this IServiceCollection services, Action<ServiceOptions> setupAction) {
             if (setupAction != null) {
                 services.Configure(setupAction);
             }
