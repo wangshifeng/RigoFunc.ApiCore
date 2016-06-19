09dd8ebc44d42e110ae2ef9f31721e98761181ee
RigoFunc <rigofunc@outlook.com>
2016-06-19 11:48:14 +0800

    feat: more meaningful logic for Api Exception and Api Result

--- src/RigoFunc.ApiCore/Filters/ApiExceptionFilterAttribute.cs
+++ src/RigoFunc.ApiCore/Filters/ApiExceptionFilterAttribute.cs
@@ -16,67 +16,99 @@ namespace RigoFunc.ApiCore.Filters {
     /// </summary>
     public class ApiExceptionFilterAttribute : ExceptionFilterAttribute {
         /// <summary>
-        /// Calls when an exception occurs.
+        /// Called when [exception].
         /// </summary>
-        /// <param name="context"></param>
-        public override void OnException(ExceptionContext context) {
+        /// <param name="context">The context.</param>
+        public sealed override void OnException(ExceptionContext context) {
             try {
                 var loggerFactory = context.HttpContext.RequestServices.GetService<ILoggerFactory>();
                 var logger = loggerFactory.CreateLogger<ApiExceptionFilterAttribute>();
                 var env = context.HttpContext.RequestServices.GetService<IHostingEnvironment>();
 
                 var exception = context.Exception;
+                // log error
+                logger.LogError(exception.ToString());
 
                 string device = context.HttpContext.Request.Headers["Device"];
                 if (!string.IsNullOrEmpty(device)) {
-                    if (device.Equals("ios", StringComparison.OrdinalIgnoreCase)) {
-                        // special logic for IOS
+                    if (device.Equals("ios", StringComparison.OrdinalIgnoreCase) && HandleExceptionForIOS(env, context)) {
+                        return;
                     }
-                    else if (device.Equals("android", StringComparison.OrdinalIgnoreCase)) {
-                        var apiResult = ApiResult.BadRequest(exception.Message);
-                        if (env.IsDevelopment()) {
-                            apiResult.Debug = GetApiDoc(context);
-                        }
-
-                        context.Result = new JsonResult(apiResult);
+                    else if (device.Equals("android", StringComparison.OrdinalIgnoreCase) && HandleExceptionForAndroid(env, context)) {
+                        return;
                     }
                 }
-                else {
-                    var json = new JObject();
 
-                    // Error message.
-                    json.Add("ErrorMessage", exception.Message);
+                // Bad Request
+                context.HttpContext.Response.StatusCode = 400;
 
-                    // Api documentation.
-                    if (exception is ArgumentNullException && env.IsDevelopment()) {
-                        json.Add("ApiDoc", GetApiDoc(context));
-                    }
-
-                    // Bad Request
-                    context.HttpContext.Response.StatusCode = 400;
-
-                    // Json result.
-                    context.Result = new JsonResult(json);
+                var json = new JObject();
+                json.Add("ErrorMessage", exception.Message);
+                if (exception is ArgumentNullException && env.IsDevelopment()) {
+                    json.Add("ApiDoc", GetApiDoc(context));
                 }
 
-                // log error
-                logger.LogError(exception.ToString());
+                // Json result.
+                context.Result = new JsonResult(json);
             }
             catch {
                 base.OnException(context);
             }
         }
 
-        private JObject GetApiDoc(ExceptionContext context) {
+        /// <summary>
+        /// Special exception handle logic for Android.
+        /// </summary>
+        /// <param name="env">The hosting environment.</param>
+        /// <param name="context">The exception context.</param>
+        /// <returns><c>true</c> if had handle the exception, <c>false</c> otherwise.</returns>
+        protected virtual bool HandleExceptionForAndroid(IHostingEnvironment env, ExceptionContext context) {
+            var apiResult = ApiResult.BadRequest(context.Exception.Message);
+            if (env.IsDevelopment()) {
+                apiResult.Debug = GetApiDoc(context);
+            }
+
+            context.Result = new JsonResult(apiResult);
+
+            return true;
+        }
+
+        /// <summary>
+        /// Special exception handle logic for ios.
+        /// </summary>
+        /// <param name="env">The hosting environment.</param>
+        /// <param name="context">The exception context.</param>
+        /// <returns><c>true</c> if had handle the exception, <c>false</c> otherwise.</returns>
+        protected virtual bool HandleExceptionForIOS(IHostingEnvironment env, ExceptionContext context) => false;
+
+        /// <summary>
+        /// Gets the API documentation.
+        /// </summary>
+        /// <param name="context">The context.</param>
+        /// <returns>A instance of <see cref="JObject"/> represents the Json style documentation.</returns>
+        protected JObject GetApiDoc(ExceptionContext context) {
             var parameters = context.ActionDescriptor.Parameters;
             if (parameters.Count > 0) {
                 if (parameters.Count == 1) {
-                    return parameters[0].ParameterType.GetXDoc();
+                    var parameter = parameters[0];
+                    if (parameter.ParameterType.IsPrimitive()) {
+                        var json = new JObject();
+                        json.Add(parameter.Name, parameter.ParameterType.Name);
+                        return json;
+                    }
+                    else {
+                        return parameter.ParameterType.GetXDoc();
+                    }
                 }
                 else {
                     var json = new JObject();
                     foreach (var item in parameters) {
-                        json.Add(item.Name, item.ParameterType.GetXDoc());
+                        if (item.ParameterType.IsPrimitive()) {
+                            json.Add(item.Name, item.ParameterType.Name);
+                        }
+                        else {
+                            json.Add(item.Name, item.ParameterType.GetXDoc());
+                        }
                     }
 
                     return json;
--- src/RigoFunc.ApiCore/Filters/ApiResultFilterAttribute.cs
+++ src/RigoFunc.ApiCore/Filters/ApiResultFilterAttribute.cs
@@ -15,29 +15,45 @@ namespace RigoFunc.ApiCore.Filters {
         /// </summary>
         /// <param name="context">The context.</param>
         /// <inheritdoc />
-        public override void OnResultExecuting(ResultExecutingContext context) {
+        public sealed override void OnResultExecuting(ResultExecutingContext context) {
             string device = context.HttpContext.Request.Headers["Device"];
             if (!string.IsNullOrEmpty(device)) {
                 if (device.Equals("ios", StringComparison.OrdinalIgnoreCase)) {
-                    // special logic for IOS
+                    OnResultExecutingForIOS(context);
                 }
                 else if (device.Equals("android", StringComparison.OrdinalIgnoreCase)) {
-                    // special logic for Android
-                    if (context.Result is ObjectResult) {
-                        var objectResult = context.Result as ObjectResult;
-                        if (objectResult.Value == null) {
-                            context.Result = new ObjectResult(ApiResult.NotFound());
-                        }
-                        else {
-                            var apiResult = Activator.CreateInstance(
-                                typeof(ApiResult<>).MakeGenericType(objectResult.DeclaredType), objectResult.Value);
-                            context.Result = new ObjectResult(apiResult);
-                        }
-                    }
+                    OnResultExecutingForAndroid(context);
                 }
             }
 
             base.OnResultExecuting(context);
+        }
+
+        /// <summary>
+        /// Called when [result executing] for Android system.
+        /// </summary>
+        /// <param name="context"></param>
+        protected virtual void OnResultExecutingForAndroid(ResultExecutingContext context) {
+            // special logic for Android
+            if (context.Result is ObjectResult) {
+                var objectResult = context.Result as ObjectResult;
+                if (objectResult.Value == null) {
+                    context.Result = new ObjectResult(ApiResult.NotFound());
+                }
+                else {
+                    var apiResult = Activator.CreateInstance(
+                        typeof(ApiResult<>).MakeGenericType(objectResult.DeclaredType), objectResult.Value);
+                    context.Result = new ObjectResult(apiResult);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Called when [result executing] for IOS system.
+        /// </summary>
+        /// <param name="context"></param>
+        protected virtual void OnResultExecutingForIOS(ResultExecutingContext context) {
+            // special logic for IOS
         }
     }
 }
