082c100b8ec60e9e63a3fab31792e67f77938cbe
RigoFunc <rigofunc@outlook.com>
2016-07-05 20:04:04 +0800

    feat: add xml comments

--- src/RigoFunc.ApiCore/Services/SmsEmailServiceCollectionExtensions.cs
+++ src/RigoFunc.ApiCore/Services/SmsEmailServiceCollectionExtensions.cs
@@ -3,7 +3,16 @@ using Microsoft.Extensions.DependencyInjection.Extensions;
 using RigoFunc.ApiCore.Services;
 
 namespace Microsoft.Extensions.DependencyInjection {
+    /// <summary>
+    /// Contains extension methods to <see cref="IServiceCollection"/> for configuring Sms and email services.
+    /// </summary>
     public static class SmsEmailServiceCollectionExtensions {
+        /// <summary>
+        /// Adds the Sms and email service.
+        /// </summary>
+        /// <param name="services">The services.</param>
+        /// <param name="setupAction">The setup action.</param>
+        /// <returns>IServiceCollection.</returns>
         public static IServiceCollection AddSmsEmailService(this IServiceCollection services, Action<SmsEmailOptions> setupAction) {
             if (setupAction != null) {
                 services.Configure(setupAction);
