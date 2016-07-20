284d5d88563e08ed3062b2fe9574f1f99c6c15db
RigoFunc <rigofunc@outlook.com>
2016-07-20 17:21:24 +0800

    feat: add unit test

---
+++ src/Host/Controllers/ValuesController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using RigoFunc.ApiCore;
+
+namespace Host.Controllers {
+    [Route("api/[controller]")]
+    public class ValuesController : ApiController {
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<string> Get() {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public DateTime Get(int id) {
+            return DateTime.Now;
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]string value) {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value) {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id) {
+        }
+    }
+}
---
+++ test/RigoFunc.ApiCore.IntegrationTest/ApiTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using Host;
+using Xunit;
+
+namespace RigoFunc.ApiCore.IntegrationTest {
+    public class ApiTest {
+        private readonly TestServer _server;
+        private readonly HttpClient _client;
+
+        public ApiTest() {
+            // Arrange
+            _server = new TestServer(new WebHostBuilder()
+                .UseStartup<Startup>());
+            _client = _server.CreateClient();
+        }
+
+        [Theory]
+        [InlineData(1)]
+        public async Task Api_Return_DateTime_Formatter_Test(int id) {
+            // Act
+            var response = await _client.GetAsync($"/api/values/{id}");
+            response.EnsureSuccessStatusCode();
+
+            var responseString = await response.Content.ReadAsStringAsync();
+
+            // Assert
+            Assert.False(responseString.Contains("T"));
+        }
+    }
+}
